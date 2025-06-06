f7b83e87291b4d5ba87d9e46590ff069f37417a1
erick bastos <erick.bastos4@etec.sp.gov.br>
2025-06-06 17:48:24 -0300

    06/06

---
+++ TrabalhoPAM1/components/IMC.tsx
@@ -0,0 +1,14 @@
+import React, { useState } from "react";
+import { View } from "react-native";
+
+export default function IMC(){
+    const [peso, setPeso] = useState('');
+    const [altura, setAltura] = useState('');
+    const [imc, setImc] = useState('');
+
+    return(
+        <View>
+
+        </View>
+    );
+}
